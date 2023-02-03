a057b0006d7d61a980cbd3eca418203a62084f16
Philipp Zahn <philippzahn@Philipps-MacBook-Pro.local>
2023-02-03 14:58:01 +0100

    Fixed for two players first

--- credible-commitments/src/AMM.hs
+++ credible-commitments/src/AMM.hs
@@ -14,23 +14,24 @@ Provides basic functionality for an AMM and the open game components
 
 {-
 TODO
-- Do players submit fees for specific positions in the transaction list?
+- We probably should start with 2 transactions only to keep it as close as possible to the PD
+- We assume that only the first transaction gets a reward
 -}
 
 -----------
 -- 1. Types
 type Fee = Double
 
-type TotalFee = Double
-
 type ContractState = (Double, Double)
+
+type ID = String
 
 data SwapTransaction = Swap0 Double | Swap1 Double
   deriving (Show, Ord, Eq)
 data Result = Swap0Out () | Swap1Out ()
   deriving (Show, Ord, Eq)
 
-type Transaction = (SwapTransaction, Fee)
+type Transaction = (SwapTransaction, Fee, ID)
 
 -------------------------
 -- 2. Auxiliary functions
@@ -39,41 +40,43 @@ inRange :: Double -> Double -> Bool
 inRange _ _ = True
 
 -- Swap a given amount of tokens
-swapWithAmount :: Transaction -> ContractState -> (Result, ContractState, Fee)
-swapWithAmount ((Swap0 amt), fee) st@(reserve0, reserve1) =
+swapWithAmount :: Transaction -> ContractState -> (Result, ContractState, Fee, ID)
+swapWithAmount ((Swap0 amt), fee, id) st@(reserve0, reserve1) =
     if inRange amt reserve0
       then
           (Swap0Out (), (reserve0 + amt
           ,(reserve0 * reserve1) / (reserve0 + amt) + 1)
-          , fee)
-      else (Swap0Out (), st, fee) -- ^ The fee is paid anyways? 
-swapWithAmount ((Swap1 amt), fee) st@(reserve0, reserve1) =
+          , fee
+          , id)
+      else (Swap0Out (), st, fee, id) -- ^ The fee is paid anyways? 
+swapWithAmount ((Swap1 amt), fee, id) st@(reserve0, reserve1) =
     if inRange amt reserve0
       then
     (Swap1Out (), ((reserve0 * reserve1) / (reserve1 + amt) + 1
     ,reserve1 + amt)
-    ,fee)
-      else (Swap1Out (), st, fee) -- ^ The fee is paid anyways?
+    ,fee
+    , id)
+      else (Swap1Out (), st, fee, id) -- ^ The fee is paid anyways?
 
 -- Thread through a sequence of transactions
 -- NOTE: We assume that the first element in the list is the first transaction to be run.
 -- NOTE: The latest element in the outcomes list is the latest state.
-lsSwapsWithAmounts :: TotalFee -> ([Transaction], ContractState) -> [(Result,ContractState, TotalFee)]
-lsSwapsWithAmounts _ ([], _)                = []
-lsSeapsWithAmounts feeTotal ((x:xs), state) =
-  let (result1,stateNew, fee) = swapWithAmount x state
-      feeTotal' = feeTotal + fee
-       in (result1,stateNew, feeTotal') : lsSwapsWithAmounts feeTotal' (xs,stateNew)
+lsSwapsWithAmounts :: ([Transaction], ContractState) -> [(Result,ContractState, Fee, ID)]
+lsSwapsWithAmounts ([], _)                = []
+lsSeapsWithAmounts ((x:xs), state) =
+  let (result1,stateNew, fee, id) = swapWithAmount x state
+       in (result1,stateNew, fee, id) : lsSwapsWithAmounts (xs,stateNew)
 
 -- Create list of all possible transaction orderings
 actionSpaceCoordinator :: ([Transaction],ContractState) -> [[Transaction]]
 actionSpaceCoordinator (lsTransaction, _ ) = permutations lsTransaction
 
 -- Compute payoff for coordinator
-computePayoffCoordinator :: [(Result,ContractState, TotalFee)] -> TotalFee
+-- NOTE We assume here that only the first transaction has to pay a fee
+computePayoffCoordinator :: [(Result,ContractState, Fee, ID)] -> Fee
 computePayoffCoordinator ls =
-  let (_,_,feeTotal) = last ls
-      in feeTotal
+  let (_,_,fee,_) = head ls
+      in fee
 -- player optimizes the order of choosing what to thread through or not.
 
 -- probably good assumption that the player can inject a set of own txs
@@ -103,14 +106,14 @@ coordinator  = [opengame|
 |]
 
 
-amm feeInitial = [opengame|
+amm  = [opengame|
   inputs: lsTransaction, state ;
   feedback: ;
 
   :------:
 
   inputs : lsTransaction, state ;
-  operation : forwardFunction $  lsSwapsWithAmounts feeInitial ;
+  operation : forwardFunction $  lsSwapsWithAmounts ;
   outputs : lsOutput;
 
   :------:
@@ -135,7 +138,7 @@ payoffs = [opengame|
 |]
 
 
-completeGame feeInitial = [opengame|
+completeGame = [opengame|
   inputs: transactionsSubmitted, state ;
   feedback: ;
 
@@ -147,7 +150,7 @@ completeGame feeInitial = [opengame|
   returns : payoffCoordinator;
 
   inputs : lsTransactionOrdered, state ;
-  operation : amm feeInitial ; 
+  operation : amm ;
   outputs : lsOutput;
 
   inputs : lsOutput ;
