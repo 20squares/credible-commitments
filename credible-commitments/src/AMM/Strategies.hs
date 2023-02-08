75c3a3ebb1c9081e914da48545b1511c0f09264e
Philipp Zahn <philippzahn@Philipps-MBP.fritz.box>
2023-02-08 23:17:57 +0100

    Added max utility strategy

--- credible-commitments/src/AMM/Strategies.hs
+++ credible-commitments/src/AMM/Strategies.hs
@@ -5,6 +5,7 @@ module AMM.Strategies where
 import OpenGames.Engine.Engine
 import AMM.ActionSpaces
 import AMM.AMM
+import AMM.Payoffs
 import AMM.Types
 import Data.List (maximumBy)
 import qualified Dhall.Map as M
@@ -51,6 +52,25 @@ maxFeeStrategy = Kleisli
     chooseMaximalFee :: [MapTransactions] -> MapTransactions
     chooseMaximalFee lsOfMaps =
       fst $ maximumBy (comparing snd) [(x, snd . snd . head . M.toList $ x)| x <- lsOfMaps]
+
+-- Maximize utility of players
+maxUtilityStrategy
+  :: MapPlayerEndowment
+  -> Kleisli
+       Stochastic
+          (MapTransactions, ContractState)
+          MapTransactions
+maxUtilityStrategy endowment = Kleisli
+ (\observation -> 
+    let actionLS  = actionSpaceCoordinator observation
+        contractState     = snd observation
+        actionLS' = [(contractState,txs)| txs <- actionLS]
+        results   = [(contractState,endowment, txs, mapSwapsWithAmounts (txs,state))| (state,txs) <- actionLS']
+        utilityLS = [(txs, computePayoffPlayerMap contractState (endowment,txs,resultsTXs))| (state,endowment, txs, resultsTXs) <- results]
+        chooseMaximalUtility = fst $ maximumBy (comparing snd) [(txs, foldr (+) 0 $ fmap snd $ M.toList utility)| (txs,utility) <- utilityLS]
+        in playDeterministically $ chooseMaximalUtility)
+
+
 
 -- Composing strategy tuple
 strategyTupleMaxFee swap1 swap2 fee1 fee2  = 
