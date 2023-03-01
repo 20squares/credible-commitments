56894a6e3acf25be05080d4c85fce23d8918e569
Fabrizio Romano Genovese <fabrizio.romano.genovese@gmail.com>
2023-03-02 00:03:42 +0100

    Cosmetics.

--- credible-commitments/pics/equilibriumGraph.sh
+++ credible-commitments/pics/equilibriumGraph.sh
@@ -4,23 +4,30 @@ set output 'equilibriumGraph.gif'
 
 set terminal gif animate nooptimize delay 25 size 1500,1200
 
-set key font "Monospace Regular, 18"
+set title "Equilibria for players' swap sizes.\nNegative values denote swaps in the opposite direction." font "Monospace Regular, 30"
 
-set grid xtics mxtics ytics mytics ztics mztics
-set hidden3d back offset 1 trianglepattern 3 undefined 1 altdiagonal bentover
-set title "Equilibria for players' swap sizes. Negative values denote swaps in the opposite direction." font "Monospace Regular, 22"
+# Legend styling
+set key font "Monospace Regular, 26" spacing 3 right bottom Right
 
-set xlabel "Player 1" offset 0,-1,0 rotate parallel
-set xrange [ -50.0000 : 50.0000 ] noreverse nowriteback
-set xtics scale 1.0 nomirror offset 0,-0.5,0
+# Axes label styling
+set xlabel "Player 1" offset 0,-1,0 rotate parallel font "Monospace Regular, 24"
+set ylabel "Player 2" offset 0,-1,0 rotate parallel font "Monospace Regular, 24"
+set zlabel "Equilibrium Fee size" offset 0,-1,0 rotate parallel font "Monospace Regular, 24"
 
-set ylabel "Player 2" offset 0,-1,0 rotate parallel
+# Axes tics styling
+set xtics scale 1.0 nomirror offset 0,-1,0 font "Monospace Regular, 22"
+set ytics scale 1.0 nomirror offset 0,-1,0 font "Monospace Regular, 22"
+set ztics scale 1.0 nomirror offset 0,-1,0 font "Monospace Regular, 22"
+
+# Axes ranges
+set xrange [ -50.0000 : 50.0000 ] noreverse nowriteback 
 set yrange [ -50.0000 : 50.0000 ] noreverse nowriteback
-
-set zlabel "Equilibrium Fee size" offset 0,-1,0 rotate parallel
+set zrange [ 0.0000 : 16.0000 ] noreverse nowriteback
 
 set colorbox vertical origin screen 0.9, 0.2 size screen 0.05, 0.6 front  noinvert bdefault
 
+set grid xtics mxtics ytics mytics ztics mztics
+set hidden3d back offset 1 trianglepattern 3 undefined 1 altdiagonal bentover
 
 $grid1 << EOD
 11	-50	-40	-30	-20	-10	0	10	20	30	40	50
@@ -56,7 +63,9 @@ n=60
 do for [i=1:n]{
 
     set view 75, i*360/n
-    splot '$grid1' matrix nonuniform with lines notitle lt rgb "#FF00FF", '$grid2' matrix nonuniform with lines notitle lt rgb "#AABBFF", '$grid1' matrix nonuniform with points lt rgb "#FF00AA" title "Minimum equilibrium", '$grid2' matrix nonuniform with points lt rgb "#AABBAA" title "Maximum equilibrium"
-#    splot '$grid2' matrix nonuniform with points notitle
+    splot '$grid1' matrix nonuniform with lines  lw 1 lt rgb "#FF00FF" notitle,\
+	  '$grid2' matrix nonuniform with lines  lw 1 lt rgb "#BF40BF" notitle,\
+	  '$grid1' matrix nonuniform with points lw 4 lt rgb "#FF00AA" title "Maximum equilibrium",\
+	  '$grid2' matrix nonuniform with points lw 4 lt rgb "#CF9FFF" title "Minimum equilibrium"
 }
 set output
